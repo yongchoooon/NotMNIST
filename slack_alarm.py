4a03c360a126fcb3b5729ef151394fa1349fee17
yongchoooon <sod7050@gmail.com>
2023-02-04 22:58:40 +0900

    SlackSender: error message 수정

--- slack_alarm.py
+++ slack_alarm.py
@@ -43,77 +43,78 @@ class SlackSender:
 
       contents = []
 
-      try: 
-        if state == "start":
-          contents = (["Your training has started 🎬",
-                          "Machine name: %s" % host_name,
-                          "Title: %s" % self.title,
-                          "Starting date: %s" % start_time.strftime(self.DATE_FORMAT)])
-          dump["text"] = "\n".join(contents)
-          dump["icon_emoji"] = ":clapper:"
-        
-        elif state == "training":
-          end_time = datetime.datetime.now()
-          elapsed_time = end_time - start_time
-          contents = (["Your training is going well 🔥",
-                          "Machine name: %s" % host_name,
-                          "Title: %s" % self.title,
-                          "Starting date: %s" % start_time.strftime(self.DATE_FORMAT),
-                          "End date: %s" % end_time.strftime(self.DATE_FORMAT),
-                          "Training duration: %s" % str(elapsed_time),
-                          "-----------------------------",
-                          "Number of epochs: %s" % self.num_epoch,
-                          "Batch size: %s" % self.batch_size,
-                          "Optimizer: %s" % self.optimizer_type])
-          for arg, arg_value in self.optimizer_args.items():
-            contents.append(f"  {arg}: {arg_value}")
-          contents.append("=============================")
-          try:
-            str_value = str(value)
-            contents.append(str_value)
-            contents.append("=============================")
-          except:
-            contents.append("Title returned value: %s"% "ERROR - Couldn't str the returned value.")
-
-          dump["text"] = "\n".join(contents)
-          dump["username"] = "Knock Knock - Training"
-          dump["icon_emoji"] = ":fire:"
-
-          self.slack_plt_image_sender(plt_dir, epoch)
-
-        elif state == "end":
-          end_time = datetime.datetime.now()
-          elapsed_time = end_time - start_time
-          contents = (["Your training is complete 🎉",
-                          "Machine name: %s" % host_name,
-                          "Title: %s" % self.title,
-                          "Starting date: %s" % start_time.strftime(self.DATE_FORMAT),
-                          "End date: %s" % end_time.strftime(self.DATE_FORMAT),
-                          "Training duration: %s" % str(elapsed_time)])
-
-          dump["text"] = "\n".join(contents)
-          dump["username"] = "Knock Knock - End"
-          dump["icon_emoji"] = ":tada:"
-
-        requests.post(self.webhook_url, json.dumps(dump))
-
-      except Exception as ex:
+      # try: 
+      if state == "start":
+        contents = (["Your training has started 🎬",
+                        "Machine name: %s" % host_name,
+                        "Title: %s" % self.title,
+                        "Starting date: %s" % start_time.strftime(self.DATE_FORMAT)])
+        dump["text"] = "\n".join(contents)
+        dump["icon_emoji"] = ":clapper:"
+      
+      elif state == "training":
         end_time = datetime.datetime.now()
         elapsed_time = end_time - start_time
-        contents = (["Your training has crashed ☠️",
-                        'Machine name: %s' % host_name,
-                        'Title: %s' % self.title,
-                        'Starting date: %s' % start_time.strftime(self.DATE_FORMAT),
-                        'Crash date: %s' % end_time.strftime(self.DATE_FORMAT),
-                        'Crashed training duration: %s\n\n' % str(elapsed_time),
-                        "Here's the error:",
-                        '%s\n\n' % ex,
-                        "Traceback:",
-                        '%s' % traceback.format_exc()])
-        dump['text'] = '\n'.join(contents)
-        dump['icon_emoji'] = ':skull_and_crossbones:'
-        requests.post(self.webhook_url, json.dumps(dump))
-        raise ex
+        contents = (["Your training is going well 🔥",
+                        "Machine name: %s" % host_name,
+                        "Title: %s" % self.title,
+                        "Starting date: %s" % start_time.strftime(self.DATE_FORMAT),
+                        "End date: %s" % end_time.strftime(self.DATE_FORMAT),
+                        "Training duration: %s" % str(elapsed_time),
+                        "-----------------------------",
+                        "Number of epochs: %s" % self.num_epoch,
+                        "Batch size: %s" % self.batch_size,
+                        "Optimizer: %s" % self.optimizer_type])
+        for arg, arg_value in self.optimizer_args.items():
+          contents.append(f"  {arg}: {arg_value}")
+        contents.append("=============================")
+        try:
+          str_value = str(value)
+          contents.append(str_value)
+          contents.append("=============================")
+        except:
+          contents.append("Title returned value: %s"% "ERROR - Couldn't str the returned value.")
+
+        dump["text"] = "\n".join(contents)
+        dump["username"] = "Knock Knock - Training"
+        dump["icon_emoji"] = ":fire:"
+
+        self.slack_plt_image_sender(plt_dir, epoch)
+
+      elif state == "end":
+        end_time = datetime.datetime.now()
+        elapsed_time = end_time - start_time
+        contents = (["Your training is complete 🎉",
+                        "Machine name: %s" % host_name,
+                        "Title: %s" % self.title,
+                        "Starting date: %s" % start_time.strftime(self.DATE_FORMAT),
+                        "End date: %s" % end_time.strftime(self.DATE_FORMAT),
+                        "Training duration: %s" % str(elapsed_time)])
+
+        dump["text"] = "\n".join(contents)
+        dump["username"] = "Knock Knock - End"
+        dump["icon_emoji"] = ":tada:"
+
+      requests.post(self.webhook_url, json.dumps(dump))
+
+      # except Exception as ex:
+      #   end_time = datetime.datetime.now()
+      #   elapsed_time = end_time - start_time
+      #   contents = (["Your training has crashed ☠️",
+      #                   'Machine name: %s' % host_name,
+      #                   'Title: %s' % self.title,
+      #                   'Starting date: %s' % start_time.strftime(self.DATE_FORMAT),
+      #                   'Crash date: %s' % end_time.strftime(self.DATE_FORMAT),
+      #                   'Crashed training duration: %s\n\n' % str(elapsed_time),
+      #                   "Here's the error:",
+      #                   '%s\n\n' % ex,
+      #                   "Traceback:",
+      #                   '%s' % traceback.format_exc()])
+      #   dump['text'] = '\n'.join(contents)
+      #   dump['icon_emoji'] = ':skull_and_crossbones:'
+      #   requests.post(self.webhook_url, json.dumps(dump))
+      #   raise ex
+
 
   def slack_plt_image_sender(self, plt_dir, epoch):
       with open(plt_dir / f"plt-epoch{str(epoch)}.png", "rb") as f: ## 이미지 보내기 
@@ -127,3 +128,30 @@ class SlackSender:
             "content": f.read()
           }
       requests.post('https://slack.com/api/files.upload', headers = header, data = attachments)
+
+
+  def slack_error_sender(self, ex):
+      dump = {
+          "username": "Knock Knock",
+          "channel": self.channel
+      }
+
+      start_time = self.start_time
+      host_name = self.host_name
+
+      end_time = datetime.datetime.now()
+      elapsed_time = end_time - start_time
+      contents = (["Your training has crashed ☠️",
+                      'Machine name: %s' % host_name,
+                      'Title: %s' % self.title,
+                      'Starting date: %s' % start_time.strftime(self.DATE_FORMAT),
+                      'Crash date: %s' % end_time.strftime(self.DATE_FORMAT),
+                      'Crashed training duration: %s\n\n' % str(elapsed_time),
+                      "Here's the error:",
+                      '%s\n\n' % ex,
+                      "Traceback:",
+                      '%s' % traceback.format_exc()])
+      dump['text'] = '\n'.join(contents)
+      dump['icon_emoji'] = ':skull_and_crossbones:'
+      requests.post(self.webhook_url, json.dumps(dump))
+      raise ex
