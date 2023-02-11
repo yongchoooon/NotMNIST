84a41b8e5263e9a880d0640166b21b6d2094ec5c
yongchoooon <sod7050@gmail.com>
2023-02-11 16:41:10 +0000

    SlackSender: now i can send end message

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -135,6 +135,9 @@ class BaseTrainer:
             if epoch % self.save_period == 0:
                 self._save_checkpoint(epoch, save_best=best)
 
+        ss.slack_sender(state = 'end',
+                        value = log_for_slack)
+
     def _save_checkpoint(self, epoch, save_best=False):
         """
         Saving checkpoints
